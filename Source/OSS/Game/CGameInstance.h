42793de9301964bf041e21fa180e229dcfaa2dfb
정찬호_JCH <elvpfhhrm@gmail.com>
2024-12-23 19:54:27 +0900

    1223_MainMenu 버튼 디자인 수정 & C++ 바인딩 & 인터페이스 역참조 & 인풋모드 되돌리기 & 위젯스위처와 조인메뉴 작성
    MainMenu 버튼 디자인 수정
    - Normal, Hover, Clicked 시에 디자인 변경
    
     C++ 바인딩
    - C++ 부모 클래스를 만든 뒤 상속시킴
    - Host 버튼 클릭시에 Host되도록 함수를 바인딩
    
    인터페이스 역참조
    - 게임 인스턴스와의 상호참조를 막기 위해 인터페이스를 생성, 게임 인스턴스에 상속 시킨뒤 인터페이스로 참조함
    
    인풋모드 되돌리기
    - 인풋모드 설정 관련 코드를 위젯으로 이동  후 함수로 분리
    - 게임 인스턴스에서 호출
    
    위젯스위처와 조인메뉴 작성
    - 위젯스위처로 메인메뉴와 조인메뉴를 스위치하는 기능을 추가
    - 조인메뉴는 메인메뉴를 기반하여 텍스트박스를 추가

--- Source/OSS/Game/CGameInstance.h
+++ Source/OSS/Game/CGameInstance.h
@@ -2,29 +2,32 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Interfaces/CMenuInterface.h"
 #include "CGameInstance.generated.h"
 
 class UUserWidget;
+class UCMainMenu;
 
 UCLASS()
-class OSS_API UCGameInstance : public UGameInstance
+class OSS_API UCGameInstance : public UGameInstance, public  ICMenuInterface
 {
 	GENERATED_BODY()
 	
 public:
 	UCGameInstance();
 
-	virtual void Init();
+	virtual void Init() override;
 
 	UFUNCTION(Exec)
-	void Host();
+	void Host() override;
 
 	UFUNCTION(Exec)
-	void Join(const FString& InAddress);
+	void Join(const FString& InAddress) override;
 
 	UFUNCTION(BlueprintCallable, Exec)
 	void LoadMainMenu();
 
 private:
 	TSubclassOf<UUserWidget> MainMenuClass;
+	UCMainMenu* MainMenu;
 };
